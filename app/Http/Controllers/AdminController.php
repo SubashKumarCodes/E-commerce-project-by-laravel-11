b455b1c9165d856c99b3290673203e63de4a8350
SubashKumarCodes <subashdeveloper19@gmail.com>
2025-08-12 18:44:25 +0600

    add_brand  method,admin.brand.add route and brand_add template create

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,4 +15,8 @@ class AdminController extends Controller
         $brands = Brand::orderBy('id','DESC')->paginate(10);
         return view('admin.brands',compact('brands'));
     }
+
+    public function add_brand(){
+        return view('admin.brand_add');
+    }
 }
