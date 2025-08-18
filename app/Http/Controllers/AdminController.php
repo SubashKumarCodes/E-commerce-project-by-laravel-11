6e09db2ac66520c0ad1aef09cdc025b19b7d868f
SubashKumarCodes <subashdeveloper19@gmail.com>
2025-08-19 00:57:59 +0600

    category( edit,update,delete) method & route create. then bug fix on category_update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -139,4 +139,50 @@ class AdminController extends Controller
             $constraint->aspectRatio();
         })->save($destinationPath.'/'.$imageName);
     }
+
+    public function category_edit($id){
+        $category = Category::find($id);
+        return view('admin.category_edit', compact('category'));
+    }
+
+    public function category_update(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'slug' =>'required|unique:categories,slug,'.$request->id,
+            'image' => 'mimes:png,jpg,jpeg|max:50048'
+        ]);
+
+        $category = Category::find($request->id);
+
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
+
+        if($request->hasFile('image')){
+            if(File::exists(public_path('uploads/categories').'/'.$category->image)){
+
+                File::delete(public_path('uploads/categories').'/'.$category->image);
+            }
+
+            $image = $request->file('image');
+            $file_extention = $request->file('image')->extension();
+            $file_name = Carbon::now()->timestamp.'.'.$file_extention;
+            $this->GenerateCategoryThumbailsImage($image,$file_name);
+            $category->image = $file_name;
+        }
+
+        $category->save();
+
+        return redirect()->route('admin.categories')->with('status','Category has been updated successfully');
+    }
+
+    public function category_delete($id){
+        $category = Category::find($id);
+
+        if(File::exists(public_path('uploads/categories').'/'.$category->image)){
+            File::delete(public_path('uploads/categories').'/'.$category->image);
+        }
+
+        $category->delete();
+        return redirect()->route('admin.categories')->with('status','Category has been deleted successfully');
+    }
 }
