4ee920a16b664e80e0aeef69e8a97706d35215df
SubashKumarCodes <subashdeveloper19@gmail.com>
2025-08-18 17:43:28 +0600

    brand_delete method create, brand_delete route create and add sweetalert confirmation before deletin brand

--- resources/views/admin/brands.blade.php
+++ resources/views/admin/brands.blade.php
@@ -74,7 +74,9 @@
                                                         <i class="icon-edit-3"></i>
                                                     </div>
                                                 </a>
-                                                <form action="#" method="POST">
+                                                <form action="{{ route('admin.brand.delete',['id'=>$brand->id]) }}" method="POST">
+                                                    @csrf
+                                                    @method('delete')
                                                     <div class="item text-danger delete">
                                                         <i class="icon-trash-2"></i>
                                                     </div>
@@ -95,3 +97,25 @@
         </div>
     </div>
 @endsection
+
+@push('scripts')
+    <script>
+        $(function(){
+            $('.delete').on('click',function(e){
+                e.preventDefault();
+                var form = $(this).closest('form');
+                swal({
+                    title: "Are you sure?",
+                    text: "Once deleted, you will not be able to recover this data",
+                    type: "warning",
+                    buttons:["No","Yes"],
+                    confirmButtonColor:'#dc3545'
+                }).then(function(result){
+                    if(result){
+                        form.submit();
+                    }
+                })
+            });
+        });
+    </script>
+@endpush
