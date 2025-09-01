e096ad4243dd7552a30d4386f0739011891e30dc
SubashKumarCodes <subashdeveloper19@gmail.com>
2025-09-01 18:33:25 +0600

    product details & img showing

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -11,45 +11,30 @@
                 <div class="swiper-container">
                 <div class="swiper-wrapper">
                     <div class="swiper-slide product-single__image-item">
-                    <img loading="lazy" class="h-auto" src="assets/images/products/product_0.jpg" width="674"
+                    <img loading="lazy" class="h-auto" src="{{ asset('uploads/products/thumbnails') }}/{{ $product->image }}" width="674"
                         height="674" alt="" />
-                    <a data-fancybox="gallery" href="../images/products/product_0.html" data-bs-toggle="tooltip"
+                    <a data-fancybox="gallery" href="{{ asset('uploads/products/thumbnails') }}/{{ $product->image }}" data-bs-toggle="tooltip"
                         data-bs-placement="left" title="Zoom">
                         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <use href="#icon_zoom" />
                         </svg>
                     </a>
                     </div>
-                    <div class="swiper-slide product-single__image-item">
-                    <img loading="lazy" class="h-auto" src="assets/images/products/product_0-1.jpg" width="674"
-                        height="674" alt="" />
-                    <a data-fancybox="gallery" href="../images/products/product_0-1.html" data-bs-toggle="tooltip"
-                        data-bs-placement="left" title="Zoom">
-                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <use href="#icon_zoom" />
-                        </svg>
-                    </a>
-                    </div>
-                    <div class="swiper-slide product-single__image-item">
-                    <img loading="lazy" class="h-auto" src="assets/images/products/product_0-2.jpg" width="674"
-                        height="674" alt="" />
-                    <a data-fancybox="gallery" href="../images/products/product_0-2.html" data-bs-toggle="tooltip"
-                        data-bs-placement="left" title="Zoom">
-                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <use href="#icon_zoom" />
-                        </svg>
-                    </a>
-                    </div>
-                    <div class="swiper-slide product-single__image-item">
-                    <img loading="lazy" class="h-auto" src="assets/images/products/product_0-3.jpg" width="674"
-                        height="674" alt="" />
-                    <a data-fancybox="gallery" href="../images/products/product_0-3.html" data-bs-toggle="tooltip"
-                        data-bs-placement="left" title="Zoom">
-                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <use href="#icon_zoom" />
-                        </svg>
-                    </a>
-                    </div>
+
+                    @foreach (explode(',',$product->images) as $gimg)
+                        <div class="swiper-slide product-single__image-item">
+                        <img loading="lazy" class="h-auto" src="{{ asset('uploads/products/thumbnails') }}/{{ $gimg }}" width="674"
+                            height="674" alt="" />
+                        <a data-fancybox="gallery" href="{{ asset('uploads/products/thumbnails') }}/{{ $gimg }}" data-bs-toggle="tooltip"
+                            data-bs-placement="left" title="Zoom">
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <use href="#icon_zoom" />
+                            </svg>
+                        </a>
+                        </div>
+                    
+                    @endforeach
+
                 </div>
                 <div class="swiper-button-prev"><svg width="7" height="11" viewBox="0 0 7 11"
                     xmlns="http://www.w3.org/2000/svg">
@@ -64,14 +49,10 @@
             <div class="product-single__thumbnail">
                 <div class="swiper-container">
                 <div class="swiper-wrapper">
-                    <div class="swiper-slide product-single__image-item"><img loading="lazy" class="h-auto"
-                        src="assets/images/products/product_0.jpg" width="104" height="104" alt="" /></div>
-                    <div class="swiper-slide product-single__image-item"><img loading="lazy" class="h-auto"
-                        src="assets/images/products/product_0-1.jpg" width="104" height="104" alt="" /></div>
-                    <div class="swiper-slide product-single__image-item"><img loading="lazy" class="h-auto"
-                        src="assets/images/products/product_0-2.jpg" width="104" height="104" alt="" /></div>
-                    <div class="swiper-slide product-single__image-item"><img loading="lazy" class="h-auto"
-                        src="assets/images/products/product_0-3.jpg" width="104" height="104" alt="" /></div>
+                    <div class="swiper-slide product-single__image-item"><img loading="lazy" class="h-auto" src="{{ asset('uploads/products/thumbnails') }}/{{ $product->image }}" width="104" height="104" alt="" /></div>
+                    @foreach (explode(',',$product->images) as $gimg)
+                        <div class="swiper-slide product-single__image-item"><img loading="lazy" class="h-auto" src="{{ asset('uploads/products/thumbnails') }}/{{ $gimg }}" width="104" height="104" alt="" /></div>
+                    @endforeach
                 </div>
                 </div>
             </div>
@@ -97,7 +78,7 @@
                 </svg></a>
             </div><!-- /.shop-acs -->
             </div>
-            <h1 class="product-single__name">Lightweight Puffer Jacket With a Hood</h1>
+            <h1 class="product-single__name">{{ $product->name }}</h1>
             <div class="product-single__rating">
             <div class="reviews-group d-flex">
                 <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg">
@@ -119,12 +100,16 @@
             <span class="reviews-note text-lowercase text-secondary ms-1">8k+ reviews</span>
             </div>
             <div class="product-single__price">
-            <span class="current-price">$449</span>
+            <span class="current-price">
+                @if($product->sale_price)
+                    <s>${{ $product->regular_price }}</s>${{ $product->sale_price }}
+                @else
+                    ${{ $product->regular_price }}
+                @endif
+            </span>
             </div>
             <div class="product-single__short-desc">
-            <p>Phasellus sed volutpat orci. Fusce eget lore mauris vehicula elementum gravida nec dui. Aenean aliquam
-                varius ipsum, non ultricies tellus sodales eu. Donec dignissim viverra nunc, ut aliquet magna posuere
-                eget.</p>
+            <p>{{$product->short_description}}</p>
             </div>
             <form name="addtocart-form" method="post">
             <div class="product-single__addtocart">
@@ -177,15 +162,15 @@
             <div class="product-single__meta-info">
             <div class="meta-item">
                 <label>SKU:</label>
-                <span>N/A</span>
+                <span>{{ $product->SKU }}</span>
             </div>
             <div class="meta-item">
                 <label>Categories:</label>
-                <span>Casual & Urban Wear, Jackets, Men</span>
+                <span>{{ $product->category->name }}</span>
             </div>
             <div class="meta-item">
                 <label>Tags:</label>
-                <span>biker, black, bomber, leather</span>
+                <span>N/A</span>
             </div>
             </div>
         </div>
@@ -210,34 +195,7 @@
             <div class="tab-pane fade show active" id="tab-description" role="tabpanel"
             aria-labelledby="tab-description-tab">
             <div class="product-single__description">
-                <h3 class="block-title mb-4">Sed do eiusmod tempor incididunt ut labore</h3>
-                <p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-                in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus
-                error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
-                inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <div class="row">
-                <div class="col-lg-6">
-                    <h3 class="block-title">Why choose product?</h3>
-                    <ul class="list text-list">
-                    <li>Creat by cotton fibric with soft and smooth</li>
-                    <li>Simple, Configurable (e.g. size, color, etc.), bundled</li>
-                    <li>Downloadable/Digital Products, Virtual Products</li>
-                    </ul>
-                </div>
-                <div class="col-lg-6">
-                    <h3 class="block-title">Sample Number List</h3>
-                    <ol class="list text-list">
-                    <li>Create Store-specific attrittbutes on the fly</li>
-                    <li>Simple, Configurable (e.g. size, color, etc.), bundled</li>
-                    <li>Downloadable/Digital Products, Virtual Products</li>
-                    </ol>
-                </div>
-                </div>
-                <h3 class="block-title mb-0">Lining</h3>
-                <p class="content">100% Polyester, Main: 100% Polyester.</p>
+                {{ $product->description }}
             </div>
             </div>
             <div class="tab-pane fade" id="tab-additional-info" role="tabpanel" aria-labelledby="tab-additional-info-tab">
